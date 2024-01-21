90cd30b20e75ce3591e76b218358f7a6dd6b2a2a
JaeHun Leem <80560040+walloonam@users.noreply.github.com>
2024-01-21 21:51:14 +0900

    [level 2] Title: 올바른 괄호, Time: 2.85 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,14 @@
+function solution(s){
+    let count = 0;
+    if(s[0] === ')' || s[s.length-1] === '('){
+            return false;
+        }
+    for(let i=0; i<s.length;i++){
+        
+        count += (s[i] === '('?1:-1);
+        if(count < 0){
+            return false;
+        }
+    }
+    return count === 0? true: false;
+}

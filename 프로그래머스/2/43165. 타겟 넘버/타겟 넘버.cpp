32dcf28cbceb1d6e467227cf97f380ce28c140d7
JaeHun Leem <80560040+walloonam@users.noreply.github.com>
2024-03-05 17:16:27 +0900

    [level 2] Title: 타겟 넘버, Time: 0.58 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -10,25 +10,16 @@ void re(vector<int> numbers, int target, int index, int sum) {
     if (index == numbers.size()) {
         if (sum == target) {
             count1++;
-            
         }
-
     }
     else {
         re(numbers, target, index + 1, sum + numbers[index]);
         re(numbers, target, index + 1, sum - numbers[index]);
     }
-
-
 }
 
 
 int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    int index = 0;
-    int sum = 0;
-    re(numbers,target,index,sum);
-    
-
+    re(numbers, target, 0, 0);
     return count1;
 }
